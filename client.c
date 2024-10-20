10cbf560811553f6cdf0ab2fa46c612571ea9a70
VM <chsieh06@usc.edu>
2024-10-20 16:22:14 -0700

    tcpv2

--- client.c
+++ client.c
@@ -5,17 +5,20 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
 int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
+    char buffer[BUFFER_SIZE] = {0};
     
     // 創建 socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
     
     // 設置 server 地址和端口
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
+    serv_addr.sin_port = htons(PORT);
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
     
     // 連接 server
@@ -25,7 +28,7 @@ int main() {
     send(sock, "Hello from client", strlen("Hello from client"), 0);
     
     // 接收 server 的回應
-    read(sock, buffer, 1024);
+    recv(sock, buffer, BUFFER_SIZE, 0);
     printf("Message from server: %s\n", buffer);
     
     // 關閉連接
