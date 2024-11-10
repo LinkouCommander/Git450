b6326baa22e9ac8ce1fe7e9362c6976feb3f0097
Brian <chsieh06@usc.edu>
2024-11-10 14:44:52 -0800

    serverA_v15

--- Git450/serverM.c
+++ Git450/serverM.c
@@ -11,25 +11,20 @@
 
 int set_udp_socket() {
     int sockfd;
-    struct sockaddr_in address;
-
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("UDP Socket creation failed");
         exit(EXIT_FAILURE);
     }
-
     return sockfd;
 }
 
 int set_tcp_socket() {
     int sockfd;
     struct sockaddr_in address;
-    
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("TCP Socket creation failed");
         exit(EXIT_FAILURE);
     }
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(TCP_PORT);
@@ -39,22 +34,19 @@ int set_tcp_socket() {
         close(sockfd);
         exit(EXIT_FAILURE);
     }
-
-    if(listen(sockfd, 3) < 0) {
+    if (listen(sockfd, 3) < 0) {
         perror("Listen failed");
         close(sockfd);
         exit(EXIT_FAILURE);
     }
-
     return sockfd;
 }
 
 int set_tcp_client_socket(int tcp_server_socket) {
     struct sockaddr_in address;
     socklen_t addr_len = sizeof(address);
-    int sockfd;
-
-    if ((sockfd = accept(tcp_server_socket, (struct sockaddr*)&address, (socklen_t*)&addr_len)) < 0) {
+    int sockfd = accept(tcp_server_socket, (struct sockaddr*)&address, &addr_len);
+    if (sockfd < 0) {
         perror("TCP Accept failed");
         exit(EXIT_FAILURE);
     }
@@ -63,79 +55,56 @@ int set_tcp_client_socket(int tcp_server_socket) {
 
 int main() {
     int udp_socket = set_udp_socket();
+    struct sockaddr_in udp_address;
+    memset(&udp_address, 0, sizeof(udp_address));
+    udp_address.sin_family = AF_INET;
+    udp_address.sin_addr.s_addr = INADDR_ANY;
+    udp_address.sin_port = htons(UDP_PORT);
 
-    struct sockaddr_in address;
-    memset(&address, 0, sizeof(address));
-
-    address.sin_family = AF_INET; // IPv4
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(UDP_PORT);
-
-    // 綁定 socket 到指定的地址和 port
-    if (bind(udp_socket, (const struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(udp_socket, (const struct sockaddr *)&udp_address, sizeof(udp_address)) < 0) {
         perror("UDP Bind failed");
         close(udp_socket);
         exit(EXIT_FAILURE);
     }
 
     int tcp_server_socket = set_tcp_socket();
-
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
 
     printf("Server is running...\n");
 
-    while(1) {
-        char buffer[BUFFER_SIZE] = {0};
-        char username[100];
-        char password[100];
-
-        memset(buffer, 0, BUFFER_SIZE);
+    while (1) {
+        char buffer[BUFFER_SIZE];
+        char username[100], password[100];
 
         int tcp_client_socket = set_tcp_client_socket(tcp_server_socket);
         printf("TCP client connected.\n");
 
-        if(recv(tcp_client_socket, buffer, BUFFER_SIZE, 0) < 0) {
-            perror("TCP Receive failed");
-            exit(EXIT_FAILURE);
-        }
+        // 接收 username
+        recv(tcp_client_socket, buffer, BUFFER_SIZE, 0);
         strcpy(username, buffer);
-        printf("client username: %s\n", buffer);
-        memset(buffer, 0, BUFFER_SIZE);
+        printf("Received username: %s\n", username);
 
-        if(recv(tcp_client_socket, buffer, BUFFER_SIZE, 0) < 0) {
-            perror("TCP Receive failed");
-            exit(EXIT_FAILURE);
-        }
+        // 接收 password
+        recv(tcp_client_socket, buffer, BUFFER_SIZE, 0);
         strcpy(password, buffer);
-        printf("client password: %s\n", buffer);
-        memset(buffer, 0, BUFFER_SIZE);
+        printf("Received password: %s\n", password);
 
-        // if(recvfrom(udp_socket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len) < 0) {
-        //     perror("UDP Receive failed\n");
-        //     exit(EXIT_FAILURE);
-        // }
-        // memset(buffer, 0, BUFFER_SIZE);
-        // printf("Received message from UDP client: %s\n", buffer);
+        // 等待 UDP Client 發送初始訊息以獲取其地址
+        recvfrom(udp_socket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len);
 
-        if(strcmp(username, "guest") != 0 && strcmp(password, "guest") != 0) {
-            printf("Not Guest\n");
-            sendto(udp_socket, username, strlen(username), 0, (const struct sockaddr *)&client_addr, client_len);
-            sendto(udp_socket, password, strlen(password), 0, (const struct sockaddr *)&client_addr, client_len);
-            printf("Response sent to UDP client\n");
-            // // send(tcp_client_socket, udp_response, strlen(udp_response), 0);
-            // // printf("Response sent to TCP client\n");
+        // 驗證並回覆 UDP Client
+        if (strcmp(username, "guest") != 0 && strcmp(password, "guest") != 0) {
+            sendto(udp_socket, username, strlen(username), 0, (struct sockaddr *)&client_addr, client_len);
+            sendto(udp_socket, password, strlen(password), 0, (struct sockaddr *)&client_addr, client_len);
+            printf("Sent response to UDP client.\n");
+        } else {
+            printf("Guest access detected.\n");
         }
-        else {
-            printf("Guest\n");
-        }
-
-        const char *tcp_response = "Hello TCP\n";
-        send(tcp_client_socket, tcp_response, strlen(tcp_response), 0);
-
-
         close(tcp_client_socket);
     }
+
     close(udp_socket);
     close(tcp_server_socket);
+    return 0;
 }
